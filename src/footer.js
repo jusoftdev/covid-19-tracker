d36cded0edefd8d235d29c55b894e5e929b4968a
juhom205 <80626044+juhom205@users.noreply.github.com>
2021-10-13 19:28:21 +0200

    Add files via upload

---
+++ src/footer.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+const Footer = () => (
+    <div className="footer">
+        <center><p>Powered by <a href="https://github.com/jusoftdev/covid-19-tracker">JuSoft</a> & <a href="https://disease.sh/">disease</a> API</p></center>
+    </div>
+);
+
+export default Footer;
